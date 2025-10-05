6fc45784cb41d4f29b42ad8e8407471764379fb3
ashraf9932 <ashraf.neizk@gmail.com>
2025-10-05 23:53:43 +0300

    init

---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+const Nav = () => {
+    return (
+        <nav className="filter-blur-3 content-contain fixed top-5 left-0 right-0 z-50 flex h-16 w-full items-center justify-between p-2 bg-[#ffffff38]">
+            Hello world
+        </nav>
+    )
+}
+export default Nav
